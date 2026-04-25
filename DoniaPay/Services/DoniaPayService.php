69efc00b8239f2e3058f1eca9c411abd67f2db42
Doniapay <sattikgamer409@gmail.com>
2026-04-25 13:32:44 +0600

    Update DoniaPayService.php

--- DoniaPay/Services/DoniaPayService.php
+++ DoniaPay/Services/DoniaPayService.php
@@ -5,14 +5,28 @@ class DoniaPayService
 {
     protected $apiKey;
     protected $apiUrl;
+    protected $apiDomain = "doniapay.com";
     public function __construct()
     {
         $this->apiKey = config('doniapay.api_key');
         $this->apiUrl = config('doniapay.api_url');
+        if (strpos($this->apiUrl, $this->apiDomain) === false) {
+            throw new \Exception("Security Alert: Invalid Api.");
+        }
     }
-    public function makePayment($data)
+    public function makePayment($genericData)
     {
-        $payload = base64_encode(json_encode($data));
+        $mappedData = [
+            "dn_su"  => $genericData['success_url'] ?? '',
+            "dn_cu"  => $genericData['cancel_url'] ?? '',
+            "dn_wu"  => $genericData['success_url'] ?? '',
+            "dn_am"  => $genericData['amount'] ?? '0',
+            "dn_cn"  => $genericData['name'] ?? 'Customer',
+            "dn_ce"  => $genericData['email'] ?? '',
+            "dn_mt"  => $genericData['meta'] ?? json_encode([]),
+            "dn_rt"  => "GET"
+        ];
+        $payload = base64_encode(json_encode($mappedData));
         $signature = hash_hmac('sha256', $payload, $this->apiKey);
         $response = Http::withHeaders([
             "X-Signature-Key" => $this->apiKey,
